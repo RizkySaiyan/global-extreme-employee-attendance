2e4a71740fcfae29143b159aca158ab5aac4fb67
Rizky Ramadhan <rizkyrmdhn09@gmail.com>
2024-06-14 09:51:35 +0800

    add activity type

--- app/Services/Constant/Activity/ActivityType.php
+++ app/Services/Constant/Activity/ActivityType.php
@@ -11,10 +11,14 @@ class ActivityType extends BaseCodeName
     const EMPLOYEE = 'employee';
     const SHIFT = 'shift';
     const PUBLIC_HOLIDAY = 'public_holiday';
+    const LEAVE = 'Leave';
 
     const OPTION = [
         self::GENERAL,
         self::COMPONENT,
-        self::EMPLOYEE
+        self::EMPLOYEE,
+        self::SHIFT,
+        self::PUBLIC_HOLIDAY,
+        self::LEAVE
     ];
 }
